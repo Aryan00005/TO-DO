5d89a598c8b7092733648fd1d48d8b097377dcb9
VRUNDKUAMR PATEL <vrund.rakesh1412@gmail.com>
2026-01-16 11:56:19 +0530

    Add new Netlify URL to CORS

--- todo-multiuser-backend/server.js
+++ todo-multiuser-backend/server.js
@@ -14,6 +14,7 @@ app.use(cors({
   origin: [
     process.env.FRONTEND_URL || 'http://localhost:3000',
     'https://dulcet-custard-82202d.netlify.app',
+    'https://stupendous-nougat-5e23b1.netlify.app',
     'http://localhost:3000',
     'http://localhost:5173',
     'http://localhost:5174',
